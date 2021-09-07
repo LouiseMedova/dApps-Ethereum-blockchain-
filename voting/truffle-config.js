31ce0a55b3a22c5f9b62774cacfede3afe27f96c
LouisaMedova <luiza.medova@phystech.edu>
2021-09-07 16:23:12 +0300

    initial commit

---
+++ voting/truffle-config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+   development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  compilers: {
+    solc: {
+      version: '0.5.2'
+    }
+  }
+};
